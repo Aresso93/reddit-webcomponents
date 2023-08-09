4689eaec92d1bec77502e87bb2cfff3157bb40b1
Aresso93 <ares.fiumicelli@gmail.com>
2023-08-09 16:30:22 +0200

    progressi, seppur lenti

--- other-functions.js
+++ other-functions.js
@@ -13,15 +13,57 @@ submitButton.addEventListener('click', function(event){
         if (checkbox.checked) {
             atLeastOneSelected = true;
         }
-    })
-    if (atLeastOneSelected) {
-        dialog.close()
-    } else {
+    });
+
+        if (atLeastOneSelected) {
+        updateNavbar(checkboxes);
+        dialog.close();
+        } else {
         alert('Seleziona almeno un subreddit!')
-    }
-})
+        }
+    });
+
+const dialogButton = document.getElementById('dialog-btn')
+dialogButton.addEventListener('click', () => {
+    dialog.showModal();
+    });
+}
+
+function updateNavbar(checkboxes){
+    console.log('SHORYUKEN!!!');
+    const selectedSubreddits = [];
+    checkboxes.forEach(checkbox => {
+        if (checkbox.checked){
+            selectedSubreddits.push(checkbox.value);
+        }
+    });
+
+
+    const customHeader = document.querySelector('#header');
+    customHeader.innerHTML = '';
+    selectedSubreddits.forEach(subreddit => {
+        const link = document.createElement('a');
+        link.appendChild(document.createTextNode(`r/${subreddit} - `))
+        link.href = `./${subreddit}.html`;
+        customHeader.appendChild(link);
+    })
+
+
+    return customHeader;
+
 }
 
 const dialogButton = document.getElementById('dialog-btn')
 const dialog = document.querySelector('dialog')
 dialogButton.addEventListener('click', () => {dialog.showModal()})
+
+
+// const navbarContainer = document.querySelector('.tatsumaki'); // Cambia la selezione in base al tuo setup
+//     navbarContainer.innerHTML = ''; // Rimuovi il contenuto esistente
+
+//     const navbar = document.createElement('ul'); // O <div>
+//     const listItem = document.createElement('li');
+//     listItem.textContent = 'Test Subreddit';
+//     navbar.appendChild(listItem);
+
+//     navbarContainer.appendChild(navbar);
--- web-components/super-header-component.js
+++ web-components/super-header-component.js
@@ -19,6 +19,53 @@ class SuperHeaderComponent extends HTMLElement{
         const h1 = document.createElement('h1');
         h1.appendChild(document.createTextNode(this.getAttribute('super-title')))
         this.shadowRoot.appendChild(h1);
+
+        // const navbar = document.createElement('nav');
+        // const linkMain = document.createElement('a')
+        // linkMain.appendChild(document.createTextNode('Torna alla pagina principale ' + '- '))
+        // linkMain.href = "./index.html"
+        // const link1 = document.createElement('a')
+        // link1.appendChild(document.createTextNode('r/aww ' + '- '))
+        // link1.href = "./aww.html"
+        // const link2 = document.createElement('a')
+        // link2.appendChild(document.createTextNode('r/italy '+ '- '))
+        // link2.href = "./italy.html"
+        // const link3 = document.createElement('a')
+        // link3.appendChild(document.createTextNode('r/Minecraft '+ '- '))
+        // link3.href = "./minecraft.html"
+        // const link4 = document.createElement('a')
+        // link4.appendChild(document.createTextNode('r/fromsoftware '+ '- '))
+        // link4.href = "./fromsoftware.html"
+        // const link5 = document.createElement('a')
+        // link5.appendChild(document.createTextNode('r/ProgrammerHumor '+ '- '))
+        // link5.href = "./programmerhumor.html"
+        // const link6 = document.createElement('a')
+        // link6.appendChild(document.createTextNode('r/cats '+ '- '))
+        // link6.href = "./cats.html"
+        // const link7 = document.createElement('a')
+        // link7.appendChild(document.createTextNode('r/StreetFighter '+ '- '))
+        // link7.href = "./streetfighter.html"
+        // const link8 = document.createElement('a')
+        // link8.appendChild(document.createTextNode('r/beer '+ '- '))
+        // link8.href = "./beer.html"
+        // const link9 = document.createElement('a')
+        // link9.appendChild(document.createTextNode('r/tipofmytongue '+ '- '))
+        // link9.href = "./tipofmytongue.html"
+        // const link10 = document.createElement('a')
+        // link10.appendChild(document.createTextNode('r/explainlikeimfive'))
+        // link10.href = "./explainlikeimfive.html"
+        // navbar.appendChild(linkMain)
+        // navbar.appendChild(link1)
+        // navbar.appendChild(link2)
+        // navbar.appendChild(link3)
+        // navbar.appendChild(link4)
+        // navbar.appendChild(link5)
+        // navbar.appendChild(link6)
+        // navbar.appendChild(link7)
+        // navbar.appendChild(link8)
+        // navbar.appendChild(link9)
+        // navbar.appendChild(link10)
+        // this.shadowRoot.appendChild(navbar)
     }
 }
 
